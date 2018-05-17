c4fb6c931e7c7322cc0e079826db57f775343396
Riccardo Pittau <elfosardo@gmail.com>
2018-05-17 11:03:20 +0200

    printing high temp values as int

--- pr2-data-visualization/ch16_downloading_data/highs_lows.py
+++ pr2-data-visualization/ch16_downloading_data/highs_lows.py
@@ -1,9 +1,15 @@
 import csv
 
-
+# Get high temperatures from file.
 filename = 'sitka_weather_07-2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+print(highs)
