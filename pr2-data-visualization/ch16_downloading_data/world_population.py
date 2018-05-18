397d1b4694722c8442f0cabb301f16d5b6c3be63
Riccardo Pittau <elfosardo@gmail.com>
2018-05-18 11:17:19 +0200

    testing country codes

--- pr2-data-visualization/ch16_downloading_data/world_population.py
+++ pr2-data-visualization/ch16_downloading_data/world_population.py
@@ -1,5 +1,7 @@
 import json
 
+from country_codes import get_country_code
+
 # Load the data into a list.
 filename = 'population_data.json'
 with open(filename) as f:
@@ -9,4 +11,8 @@ with open(filename) as f:
         if pop_dict['Year'] == '2010':
             country_name = pop_dict['Country Name']
             population = int(float(pop_dict['Value']))
-            print(country_name + ": " + str(population))
+            code = get_country_code(country_name)
+            if code:
+                print(code + ": " + str(population))
+            else:
+                print('ERROR - ' + country_name)
