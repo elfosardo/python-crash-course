95984ffb452fc3f18841d34021459ada5b42bae7
Riccardo Pittau <elfosardo@gmail.com>
2018-05-31 17:30:13 +0200

    adding verify option to cert path for ubuntu

--- pr2-data-visualization/ch17_working_with_apis/hn_submissions.py
+++ pr2-data-visualization/ch17_working_with_apis/hn_submissions.py
@@ -6,10 +6,7 @@ from operator import itemgetter
 
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 
-s = requests.Session()
-s.verify = '/etc/ssl/certs/ca-certificates.crt'
-
-r = requests.get(url)
+r = requests.get(url, verify='/etc/ssl/certs/ca-certificates.crt')
 print("Status code:", r.status_code)
 
 # Process information about each submission.
@@ -19,7 +16,8 @@ for submission_id in submission_ids[:30]:
     # Make a separate API call for each submission.
     url = ('https://hacker-news.firebaseio.com/v0/item/' +
            str(submission_id) + '.json')
-    submission_r = requests.get(url)
+    submission_r = requests.get(url,
+                                verify='/etc/ssl/certs/ca-certificates.crt')
     print(submission_r.status_code)
     response_dict = submission_r.json()
 
