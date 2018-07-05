708992b08931c8a8a8b24ffd752f146f15f24e4d
Riccardo Pittau <elfosardo@gmail.com>
2018-07-05 16:15:52 +0200

    adding views for single entries in topics

--- pr3_web_apps/learning_logs/urls.py
+++ pr3_web_apps/learning_logs/urls.py
@@ -8,4 +8,10 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-]
+
+    # Show all topics
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topics/(?P<topic_id>\d+)/', views.topic, name='topic')
+ ]
